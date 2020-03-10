94b4818d62702ace39772159ebaa99fd8b50f8df
Zahid Ali <46484008+zahidalidev@users.noreply.github.com>
2020-03-10 05:57:21 -0700

    author changes

--- theory/simple.c
+++ theory/simple.c
@@ -13,8 +13,6 @@ int simple_init(void)
 {
    start = jiffies;
    printk(KERN_INFO "loading kernel module\n");
-   printk(KERN_INFO "GOLDEN RATIO PRIME: %llu\n", GOLDEN_RATIO_PRIME);
-   printk(KERN_INFO "HZ:%d entering jiffies: %lu\n", HZ, jiffies);
    return 0;
 }
 
@@ -23,9 +21,6 @@ void simple_exit(void)
 {
    end = jiffies;
    printk(KERN_INFO "Removing Kernel Module\n");
-   printk(KERN_INFO "gcd(21,93)= %lu\n", gcd(21,93));
-   printk(KERN_INFO "HZ:%d exit jiffies: %lu\n", HZ, jiffies);
-   printk(KERN_INFO "time lapsed: %lu\n", (end - start) / HZ);
 }
 
 /* Macros for registering module entry and exit points. */
@@ -33,4 +28,4 @@ module_init(simple_init);
 module_exit(simple_exit);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Module");
-MODULE_AUTHOR("Shengjun Ma");
+MODULE_AUTHOR("Zahid Ali");
