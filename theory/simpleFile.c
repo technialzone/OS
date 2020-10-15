18901ffc1154fdca9e0c16c2b117b8d124eed30f
hafizshahiddev <72939899+hafizshahiddev@users.noreply.github.com>
2020-10-16 03:05:43 +0500

    c file

---
+++ theory/simpleFile.c
@@ -0,0 +1,31 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/gcd.h>
+#include <linux/hash.h>
+#include <asm/param.h>
+
+
+int start;
+int end;
+/* This function is called when the module is loaded. */
+int simple_init(void)
+{
+   start = jiffies;
+   printk(KERN_INFO "loading kernel module\n");
+   return 0;
+}
+
+/* This function is called when the module is removed. */
+void simple_exit(void)
+{
+   end = jiffies;
+   printk(KERN_INFO "Removing Kernel Module\n");
+}
+
+/* Macros for registering module entry and exit points. */
+module_init(simple_init);
+module_exit(simple_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("Zahid Ali");
